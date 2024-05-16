7daf3d157bdebdcfa9ed926e48fb6045e75b2ed2
OdhiamboMoses <odhiambomoses08@gmail.com>
2024-05-16 09:00:04 +0300

    Paragraph content update

--- js/toggler.js
+++ js/toggler.js
@@ -14,3 +14,30 @@ function toggleMenu(){
         menuToggler.style.display = 'block';
     }
 };
+
+let btn = document.getElementById("bgbtn");
+let bodycont = document.querySelector("body");
+let hd = document.querySelector("header");
+let hdbtn = document.querySelectorAll("#hdBtn");
+let bgbtn1 = document.getElementsByClassName("bgbtn1");
+let contactBtn = document.getElementById("btn-contact");
+let socials = document.getElementsByClassName("socials");
+let pcont = document.getElementsByClassName("main-content");
+
+function changeBg()
+{
+    btn.addEventListener('click', (e) => {
+        bodycont.style.backgroundColor = "#A9EB14";
+        hd.style.backgroundColor = "rgb(66, 25, 160)";
+        hdbtn.style.backgroundColor = "#A9EB14";
+        hd.style.color = "#00000";
+    }); 
+};
+function changeBg2()
+{
+    // btn.addEventListener('click', (e) => {
+    //     e.preventDefault();
+        bodycont.style.backgroundColor = "rgb(66, 25, 160)";
+        hd.style.backgroundColor = "#A9EB14";
+    ; 
+};
